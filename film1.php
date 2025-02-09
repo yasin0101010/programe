27bbdfe2969d4d8f8ce1b04a0d6e4ab8ca537a59
yasin0101010 <yasyngym2200@gmail.com>
2025-02-09 11:32:12 +0330

    verson 1.4.2

--- film1.php
+++ film1.php
@@ -13,69 +13,9 @@ session_start();
     <link rel="stylesheet" href="moves styles.css">
 </head>
 <body>
-    <div id="header"  class="flex-container">
-        <!-- قسمت بالایی سایت -->
-        
-        <div id="header_menu">
-            <button class="menu-button" onclick="toggleOffcanvas()">☰</button>
-        </div>
-        <!-- Offcanvas با محتوای RTL انگلیسی -->
-        <div class="offcanvas" id="myOffcanvas">
-            <div class="offcanvas-header">
-                <h3>Menu</h3>
-                <span class="close-btn" onclick="toggleOffcanvas()">&times;</span>
-            </div>
-            <div class="offcanvas-body">
-                <ul>
-                    <li><a href="index.html" style="color: black;">phoonix moves</a></li>
-                    <?php
-                    if (isset($_SESSION['login_check'])){?>
-                    <li><a href="logout.php" style="color: red;">logout</a></li>
-                    <?php
-                    }
-                    else{?>
-                    <li><a href="register.html" style="color: blue;">login/register</a></li>
-                    <?php
-                    }
-                    ?>
-                    
-                </ul>
-            </div>
-        </div>
-    
-        <div class="overlay" id="overlay" onclick="toggleOffcanvas()"></div>
-    
-        <script>
-            function toggleOffcanvas() {
-                const offcanvas = document.getElementById('myOffcanvas');
-                const overlay = document.getElementById('overlay');
-                
-                offcanvas.classList.toggle('active');
-                overlay.classList.toggle('active');
-                
-                document.body.style.overflow = offcanvas.classList.contains('active') ? 'hidden' : 'auto';
-            }
-        </script>
-        
-        <div class="logo-container">
-            <a href="index.php" class="logo-link">
-                <img src="logo site.png" alt="Logo" class="logo">
-            </a>
-        </div>            
-        <div id="header_space_1"> 5</div>
-        <div id="header_search">
-            <form action="/search" method="get">
-                <input type="text" name="q" placeholder="Search here..." required>
-                <button type="submit">Search</button>
-            </form>
-        </div>
-        <div id="header_space_2"> </div>
-        <div class="profile-container">
-            <a id="profile-link" href="#">
-                <img id="profile-image" src="default-user.jpg" alt="Profile Image">
-            </a>
-        </div>
-    </div>
+    <?php
+        include 'footer.html';
+    ?>
     <div id="movie_space">
         <!-- قسمت ویدیو و پلی لیست -->
 
